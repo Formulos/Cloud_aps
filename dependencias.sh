819b83b99b3cb749cd831f72e8e644f2f4e0d95e
formulos <paulotozp@Gmail.com>
2018-11-28 11:32:07 -0200

    esqueci de algumas dependencias

--- dependencias.sh
+++ dependencias.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo apt install python-pip -y
+pip install flask
 pip install flask_restful
 pip install flask_httpauth
--- dependencias_balancer.sh
+++ dependencias_balancer.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo apt install python-pip -y
+pip install flask
 pip install flask_restful
 pip install flask_httpauth
 pip install boto3
